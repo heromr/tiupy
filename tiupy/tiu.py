7b0b651400214d9a8bfd959644014b71db9989fb
HeroMR <84679204+heromr@users.noreply.github.com>
2024-04-23 19:30:51 +0300

    Update tiu.py

--- tiupy/tiu.py
+++ tiupy/tiu.py
@@ -1,4 +1,4 @@
-from httpx import AsyncClient
+from httpx import Client
 from typing import Optional, Any, Dict
 from .utils import headers, objects
 
@@ -12,50 +12,42 @@ class Tiu:
         self.request_timeout = request_timeout
         self.profile = objects.UserProfile(None)
 
-    async def __aenter__(self):
-        self.client = AsyncClient(proxies=self.proxies, timeout=self.request_timeout)
-        return self
-
-    async def __aexit__(self, exc_type, exc_value, traceback):
-        await self.client.aclose()
-    
-    async def make_request(self, method: str, endpoint: str, data: Optional[Dict[str, Any]] = None, params: Optional[Dict[str, Any]] = None):
+    def make_request(self, method: str, endpoint: str, data: Optional[Dict[str, Any]] = None, params: Optional[Dict[str, Any]] = None):
         url = self.base_url + endpoint
-        response = await self.client.request(method, url, headers=headers.Headers().headers, data=data, params=params)
-        response.raise_for_status()
-        return response
+        with Client(proxies=self.proxies, timeout=self.request_timeout) as client:
+            response = client.request(method, url, headers=headers.Headers().headers, data=data, params=params)
+            response.raise_for_status()
+            return response
     
-    async def _get_profile_info(self):
-        response = await self.make_request("GET", endpoint="/pages/home.php")
+    def _get_profile_info(self):
+        response = self.make_request("GET", endpoint="/pages/home.php")
         return response.text
     
-    async def login(self, username: str, password: str):
+    def login(self, username: str, password: str):
         data = {
             'username': username,
             'password': password,
-            'login.x': '0',  #Need to change?
-            'login.y': '0'   #Need to change?
+            'login.x': '0',  # Need to change?
+            'login.y': '0'   # Need to change?
         }
 
-        response = await self.make_request("POST", endpoint="/", data=data)
+        response = self.make_request("POST", endpoint="/", data=data)
 
         self.sid = response.cookies.get("PHPSESSID")
         headers.sid = self.sid
-        self.profile = objects.UserProfile(await self._get_profile_info())
+        self.profile = objects.UserProfile(self._get_profile_info())
         return response.status_code
     
-    async def sid_login(self, SID: str):
+    def sid_login(self, SID: str):
         self.sid = SID
         headers.sid = self.sid
-        self.profile = objects.UserProfile(await self._get_profile_info())
+        self.profile = objects.UserProfile(self._get_profile_info())
         return
     
-    async def logout(self):
-        response = await self.make_request("GET", endpoint="/pages/p999.php/")
+    def logout(self):
+        response = self.make_request("GET", endpoint="/pages/p999.php/")
         return response.status_code
     
-    async def get_courses_data(self):
-        response = await self.make_request("GET", endpoint="/pages/p103.php")
+    def get_courses_data(self):
+        response = self.make_request("GET", endpoint="/pages/p103.php")
         return objects.CourseData(response.text)
-
-    
