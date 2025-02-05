f2c13ca8d623b51384b2bde31afd6ea937546be4
HeroMR <84679204+heromr@users.noreply.github.com>
2025-02-05 10:36:13 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
 from setuptools import find_packages, setup
 
 with open("README.md", "r", encoding="utf-8") as f:
-  long_description = f.read()
+    long_description = f.read()
 
-VERSION = "0.0.11"
-DESCRIPTION = "A web scraper for TIU student system"
+VERSION = "1.1.0"
+DESCRIPTION = "A robust web scraper and API wrapper for TIU student system"
 
 setup(
     name="tiupy",
@@ -16,8 +16,11 @@ setup(
     url="https://github.com/heromr/tiupy",
     license="MIT",
     packages=find_packages(),
-    install_requires=[],
-    keywords=["tiu uni", "tishk university", "tiu scraper", "tiu API"],
+    install_requires=[
+        "httpx>=0.24.1",  # For making HTTP requests
+        "beautifulsoup4>=4.12.2"  # For parsing HTML content
+    ],
+    keywords=["tiu uni", "tishk university", "tiu scraper", "tiu API", "university system"],
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
@@ -25,4 +28,5 @@ setup(
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
     ],
+    python_requires='>=3.6',
 )
